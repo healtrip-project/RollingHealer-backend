8e69c998bd8680164de6bd64cc2b28af69c8fa0a
mudokja <mudokja@gmail.com>
2023-11-13 17:50:09 +0900

    temp:임시저장2

---
+++ src/main/java/com/ssafy/rollinghealer/auth/TokenProvider.java
@@ -0,0 +1,17 @@
+package com.ssafy.rollinghealer.auth;
+
+import org.springframework.security.core.Authentication;
+
+public class TokenProvider {
+
+	public boolean validateToken(String jwt) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Authentication getAuthentication(String jwt) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
